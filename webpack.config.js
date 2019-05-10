3d70fa93b795e19859ce9b699ced4b7d2903fa9e
CosSalt <ihosalt@gmail.com>
2019-05-10 17:55:28 +0800

    [dev] 重载webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+
+const webpack = require('af-webpack/webpack')
+module.exports = function (webpackConfig, env) {
+  webpackConfig.plugins.unshift(new webpack.DefinePlugin({
+    'process.env': {
+        NODE_ENV: process.env.NODE_ENV,
+        BUILD_TIME: Date.now(), // 打包时间
+    }
+  }))
+  return webpackConfig
+}
